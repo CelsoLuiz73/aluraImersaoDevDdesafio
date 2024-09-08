4338fcad2714619922c8e52528d086ddd72cd908
CelsoLuiz73 <oclinfo21@gmail.com>
2024-09-07 23:45:58 -0300

    primeiro commit

---
+++ dados-dc-marvel.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, conhecida como a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, trazendo o skate para o centro das atenções. Sua história de superação e paixão pelo esporte a tornou um ícone para a nova geração.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+    },
+    {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com diversas medalhas em diferentes modalidades da ginástica artística. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um símbolo do esporte nacional e um exemplo para jovens atletas.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
+    },
+    {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua trajetória inspiradora, marcada pela superação de desafios e pela busca incansável pela excelência, a tornou um exemplo para outros atletas e para a comunidade negra.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoca judoka"
+    }
+];
